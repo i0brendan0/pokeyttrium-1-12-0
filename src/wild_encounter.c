ca31a25842d08f86f24d92af4c124c5546fef214
i0brendan0 <19826742+i0brendan0@users.noreply.github.com>
2025-06-01 08:44:43 -0500

    Swarm Pokemon can hold items
    Update tv.c

--- src/wild_encounter.c
+++ src/wild_encounter.c
@@ -604,6 +604,9 @@ static bool8 SetUpMassOutbreakEncounter(u8 flags)
     for (i = 0; i < MAX_MON_MOVES; i++)
         SetMonMoveSlot(&gEnemyParty[0], gSaveBlock1Ptr->outbreakPokemonMoves[i], i);
 
+    if (gSaveBlock1Ptr->outbreakPokemonItem != ITEM_NONE)
+        SetMonData(&gEnemyParty[0], MON_DATA_HELD_ITEM, &gSaveBlock1Ptr->outbreakPokemonItem);
+
     return TRUE;
 }
 
