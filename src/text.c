3f28afecaaafe98065b3f9b491deddecfa18264e
i0brendan0 <19826742+i0brendan0@users.noreply.github.com>
2025-06-05 02:39:18 -0500

    Faster Text

--- src/text.c
+++ src/text.c
@@ -387,10 +387,10 @@ void RunTextPrinters(void)
             if (sTextPrinters[i].active)
             {
                 u16 renderCmd = RenderFont(&sTextPrinters[i]);
+                CopyWindowToVram(sTextPrinters[i].printerTemplate.windowId, COPYWIN_GFX);
                 switch (renderCmd)
                 {
                 case RENDER_PRINT:
-                    CopyWindowToVram(sTextPrinters[i].printerTemplate.windowId, COPYWIN_GFX);
                 case RENDER_UPDATE:
                     if (sTextPrinters[i].callback != NULL)
                         sTextPrinters[i].callback(&sTextPrinters[i].printerTemplate, renderCmd);
@@ -1053,6 +1053,7 @@ static u16 RenderText(struct TextPrinter *textPrinter)
     u16 currChar;
     s32 width;
     s32 widthHelper;
+    u8 repeats = 1;
 
     switch (textPrinter->state)
     {
@@ -1076,6 +1077,20 @@ static u16 RenderText(struct TextPrinter *textPrinter)
         else
             textPrinter->delayCounter = textPrinter->textSpeed;
 
+		switch (GetPlayerTextSpeed())
+		{
+			case OPTIONS_TEXT_SPEED_SLOW:
+				repeats = 1;
+				break;
+			case OPTIONS_TEXT_SPEED_MID:
+				repeats = 2;
+				break;
+			case OPTIONS_TEXT_SPEED_FAST:
+				repeats = 4;
+				break;
+		}
+		do 
+        {
         currChar = *textPrinter->printerTemplate.currentChar;
         textPrinter->printerTemplate.currentChar++;
 
@@ -1292,6 +1307,10 @@ static u16 RenderText(struct TextPrinter *textPrinter)
             else
                 textPrinter->printerTemplate.currentX += gCurGlyph.width;
         }
+        repeats--;
+        }
+        while (repeats > 0);
+        
         return RENDER_PRINT;
     case RENDER_STATE_WAIT:
         if (TextPrinterWait(textPrinter))
